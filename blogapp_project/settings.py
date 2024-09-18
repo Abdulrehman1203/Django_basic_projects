70bfdcc09980afd4804fcb676383cc4db1621372
Umer <umershahzad2003kpr@gmail.com>
2024-09-18 21:24:35 +0500

    Moved manage.py from main directory to project,to maintain the respository structure

--- blogapp_project/settings.py
+++ blogapp_project/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import psycopg2
 import os
 from datetime import timedelta
-import psycopg2
 from pathlib import Path
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
